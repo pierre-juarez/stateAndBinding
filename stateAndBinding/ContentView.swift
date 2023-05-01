8d888d1141458910766a53cfc59a967ec1ea7d69
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-05-01 00:07:30 -0500

    [add] - Example of state 🤓

--- stateAndBinding/ContentView.swift
+++ stateAndBinding/ContentView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var x = ""
+    var x1 = 2
+    
+    func sum() -> Int {
+        x = "Change value" // If the variable comes from outside, the state is used.
+        var x2 = 2 // It is created inside the function, the state is not used.
+        x2 = 4
+        return x2
+    }
+    
+    @State private var show = true
+    @State private var number = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack{
+                Button {
+                    show.toggle()
+                    number += (show) ? -1: 1
+                } label: {
+                   
+                    Image(systemName: (show) ? "heart" : "heart.fill")
+                        .foregroundColor(.red)
+                        .font(.largeTitle)
+                    Text(String(number)).bold()
+                }
+
+            }
         }
         .padding()
     }
