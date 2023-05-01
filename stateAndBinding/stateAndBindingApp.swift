318cd8a79b42a82682c51679f9ab81e3bf5f2978
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-04-30 23:50:42 -0500

    [add] - Initial commit 🚀

---
+++ stateAndBinding/stateAndBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  stateAndBindingApp.swift
+//  stateAndBinding
+//
+//  Created by Pierre Juarez U. on 30/04/23.
+//
+
+import SwiftUI
+
+@main
+struct stateAndBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
